f0019393e17dc126ed024f3cb0963b5edd5ef81d
TonyCross23 <tonychamp40@gmail.com>
2026-05-01 21:17:23 +0630

    day01

---
+++ day01/app.js
@@ -0,0 +1,102 @@
+// console.log('Hello World');
+// console.log("Ma Ma");
+
+// console.log(1 + 1); //+
+
+// console.log(2 * 5); // * is for multiplication
+
+// console.log(10 / 2); // / is for division
+
+// console.log(5 - 2); // - is for subtraction
+
+
+// number
+// let a = 10; // = assignment operator
+// let b = 20;
+
+// console.log("The value of a is:" ,a);
+// console.log("The value of b is:", b);
+
+// // console.log(a + b);
+// // console.log(a / b);
+// // console.log(a - b);
+// // console.log(a * b);
+
+// console.log(a + b);
+
+
+
+// // data type   
+// console.log(typeof a);
+
+// string
+// let name = "Ma Ma";
+
+// console.log("Hello", name);
+
+
+// // boolean
+// let isRaining = true;
+// let isSunny = false;
+
+// console.log(typeof isRaining);
+
+// let , var , const
+// let name = "Mg Mg"; //assig
+
+// const name = "Mg Mg"
+
+// var name = "Mg Mg"; //string
+
+// console.log(typeof name); //tyoeof type check
+
+// var age = 30; //integer
+
+// console.log(typeof age);
+
+// const isStudent = true;
+// console.log(typeof isStudent); //ture is 1
+
+// var num = 3.14;
+
+// console.log(typeof num);
+
+// console.log(undefined);
+
+// let x;
+// // x = 10
+// console.log(typeof x);
+
+// let y = null
+// y = 10
+// y = 20
+// console.log(y);
+
+// const b = 10;
+
+// console.log("value of b :", b);
+
+
+let a = 10; // variable declaration and assignment
+console.log("this is before change a value: ", a);
+a = 20;
+console.log("this is after change a value:", a);
+a = 30;
+console.log("this is after change a value again:", a);
+
+const b = 100;
+console.log("The value of b is :", b);
+
+const name = "Ma Ma";
+console.log("Good morning", name);
+
+var c = 50;
+console.log("This is c ", c);
+c = 100;
+console.log("This is c after change value: ", c);
+
+// let IsStudent = true;
+// let isStudent = true;
+// let is_student = true;
+
+// let is Student = true; //space is not allowed in variable name
