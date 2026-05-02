e532613e51b81529d7bf39004c93dc136cae538b
TonyCross23 <tonychamp40@gmail.com>
2026-05-02 21:32:42 +0630

    day02

---
+++ day02/app.js
@@ -0,0 +1,81 @@
+
+// let a = 10
+// let b = 20
+
+// let c = a + b
+
+// console.log("c answer is", c);
+
+// let d = a % b
+
+// console.log("d answer is", d);
+
+
+// let e = 20
+
+// // e += 5  // e = e + 5 e = 20 + 5
+
+// // e -= 10 // e = e - 10 e = 20 - 10
+
+// e ++ // e = e + 1 e = 20 + 1
+// e -- // e = e - 1 e = 20 - 1
+
+// console.log(e);
+
+// let x = 10 //number
+// let y = "10" //string
+// let z = 20 //number
+
+// console.log(x == y); // not check data type, only check value
+// console.log(x === y); // check data type and value
+
+// console.log( x != z);
+
+// console.log(x > z);
+// console.log(x < z);
+
+// console.log("Hello");
+
+// const age = 17
+
+// // ညီရင်ညီ, ကြီ
+// if (age >= 18) {
+//     console.log("allow");
+// } else {
+//     console.log("not allow");
+// }
+
+// const isMarriedWithMaMa = true
+
+// if (isMarriedWithMaMa) {
+//     console.log("i love you 3000");
+// } else {
+//     console.log(" crying ");
+// }
+
+
+// let examScore = 39
+
+// if (examScore >= 80) {
+//     console.log("D");
+// }else if (examScore >= 40) {
+//     console.log("Pass");
+// } else {
+//     console.log("Fail");
+// }
+
+let myanmar = 80
+let english = 50
+let math = 60
+
+if ( myanmar >= 40 && english >= 40 && math >= 40) {
+    console.log("Pass");
+} else if ( english >= 80) {
+    console.log("English is A");
+} else if(math >= 80) {
+    console.log("Math is A");
+} else if ( myanmar >= 80) {
+    console.log("Myanmar is A");
+} else {
+    console.log("Fail");
+}
