f027c40d6294ed6c775eb2b44824f8729aba3587
TonyCross23 <tonychamp40@gmail.com>
2026-05-05 21:48:40 +0630

    day04

---
+++ day04/app.js
@@ -0,0 +1,30 @@
+let myArray = ["Mg Mg", "Ma Ma", "Aung Aung"]
+
+// console.log("Befor add next one",myArray);
+
+// myArray.push("Hla Hla")
+
+// console.log("after pushed", myArray);
+
+// myArray.pop()
+
+// console.log("after deleted result", myArray);
+
+// myArray.shift()
+
+// console.log(myArray);
+
+// myArray.unshift("U Ba")
+
+// console.log(myArray);
+
+
+// for( let i = 0 ; i < 5; i++) {
+//     console.log(i);
+// }
+
+let x = 0
+
+for (x; x < 10; x++) {
+    console.log(x + "." + "Chee Par chin tal");
+}
---
+++ main.js
@@ -0,0 +1,53 @@
+let myLuckyNumbers = [11, 22, 33];
+
+// console.log(myLuckyNumbers.length);
+
+// myLuckyNumbers.push(44)
+// myLuckyNumbers.push(45)
+
+// myLuckyNumbers.pop()
+
+// myLuckyNumbers.shift()
+
+// myLuckyNumbers.unshift(90)
+// console.log(myLuckyNumbers);
+
+
+// for (let i = 0; i < 5; i++) {
+//     console.log(i);
+    
+// }
+
+// let myFriends = ["Aung Aung", "Kyaw Kyaw", "Su Su", "Mya Mya"];
+
+// for (let i = 0; i < myFriends.length; i++) {
+//     console.log(myFriends[i]);
+// }
+
+// let count = 1
+
+// while (count < 5) {
+//     console.log("Count is :", count);
+//     count++;
+// }
+
+// let x = 1;
+
+// do {
+//     console.log(x);
+//     // console.log("အနည်းဆုံး တစ်ခါတော့ ထွက်မယ်");
+//     x++;
+// } while (x < 5)
+
+
+// let fruits = ["Apple", "Mango", "Banana"];
+
+// for(let fruit of fruits) {
+//     console.log(fruit);
+// }
+
+// fruits.forEach(function(fruit, index) {
+//     index++ 
+//     console.log(index+ "." + fruit);
+    
+// })
