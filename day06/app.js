2bbaed658c912fd0acf19a968ad8be30d5368616
TonyCross23 <tonychamp40@gmail.com>
2026-05-07 21:02:22 +0630

    day06

---
+++ day06/app.js
@@ -0,0 +1,86 @@
+// let name = "Mg Mg"
+// console.log(name);
+
+// let age = 25
+
+// console.log(age);
+
+// function greeting() {
+//     console.log("Hello World");
+// }
+
+// greeting()
+
+// function add(a,b) {
+//     return a+b
+// }
+
+// const result = add(10,20)
+
+// console.log(result);
+
+// let a = 10
+// let b = 20
+
+// console.log(a+b);
+
+// function Hello(a,b) {
+//     return a/b
+// }
+
+// const result = Hello(20,30)
+
+// console.log(result);
+
+// function Name(name) {
+//     return name
+// }
+// const result = Name("Mg Mg")
+
+// if( result == "Ma Ma") {
+//     console.log("Female");
+// } else {
+//     console.log(result);
+// }
+
+// const name = ["mg mg", "ma ma"] 
+
+// const car = {
+//     brand : "NISSAN",
+//     model : "GTR",
+//     isEv : false
+// }
+
+// console.log(car["brand"]);
+// console.log(car.model);
+
+// const student = {
+//     name : "Mg Mg",
+//     grade : 10,
+//     age : 15,
+//     gender : "Male"
+// }
+
+// console.log(student.name);
+// console.log(student.name, student.gender, student.age);
+// console.log(student["name"]);
+
+// const result = [
+//     {
+//         name: "Mg Mg",
+//         age: 24,
+//     }, 
+//     {
+//         name: "Aung Aung",
+//         age: 25
+//     },
+//     {
+//         name: "Ma Ma",
+//         age: 30,
+//         family: {
+//             name: ""
+//         }
+//     }
+// ]
+
+// console.log(result);
---
+++ day06/main.js
@@ -0,0 +1,45 @@
+// // array object
+// const users = [
+//     {
+//         name: "Mg Mg",
+//         email: "mgmg@gmail.com",
+//         age: 20
+//     },
+//     {
+//         name: "Ma Ma",
+//         email: "mama@gmail.com",
+//         age: 30
+//     },
+//     {
+//         name: "Moe Moe",
+//         email: "moemoe@gmail.com",
+//         age: 28
+//     },
+//     {
+//         name: "Min Min",
+//         email: "minmin@gmail.com",
+//         age: 25
+//     },
+// ]
+
+// // console.log(users[1].email);
+
+// for(let user of users) {
+//     console.log(user.email);
+// }
+
+const myobj = {
+    name: "Mg Mg",
+    age: 25,
+    family: {
+        father_name: "U Ba",
+        mom_name: "Daw Mya",
+        sister: "Mya Mya",
+        pet: {
+            dog_name: "Aung Black",
+            cat_name: "Shwe Wa"
+        }
+    }
+}
+
+console.log(myobj);
